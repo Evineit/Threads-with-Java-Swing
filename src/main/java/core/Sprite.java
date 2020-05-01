9510de852aabb342bc616ecfea750f9189bf0fbe
Kevin R <seidaikevin@gmail.com>
2020-05-01 02:38:30 -0500

    testing sprites

---
+++ src/main/java/core/Sprite.java
@@ -0,0 +1,35 @@
+package core;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Sprite {
+
+    private static BufferedImage spriteSheet;
+    private static final int TILE_SIZE = 32;
+
+    public static BufferedImage loadSprite(String file) {
+
+        BufferedImage sprite = null;
+
+        try {
+            sprite = ImageIO.read(new File("C:\\Users\\kevin\\Google Drive\\proyectos\\hilosu3\\src\\main\\resources\\wFRJj.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sprite;
+    }
+
+    public static BufferedImage getSprite(int xGrid, int yGrid) {
+
+        if (spriteSheet == null) {
+            spriteSheet = loadSprite("AnimationSpriteSheet");
+        }
+
+        return spriteSheet.getSubimage(xGrid * TILE_SIZE, yGrid * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+
+}
